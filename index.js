cd5315b2f8b3698de5330e6fdb120ad9d7e35fba
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-03-27 10:49:57 -0500

    style: js-beautify formatting

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-console.log((1 >= NaN)||(1 <= NaN));
-void (async function Index() {
-  let { zwriteFileSync, zreadFileSync } = await import("./zfs.mjs");
+console.log((1 >= NaN) || (1 <= NaN));
+void(async function Index() {
+  let {
+    zwriteFileSync,
+    zreadFileSync
+  } = await import("./zfs.mjs");
 
-  let scriptText = zreadFileSync("./pako.js", { encoding: "utf-8" });
+  let scriptText = zreadFileSync("./pako.js", {
+    encoding: "utf-8"
+  });
   scriptText = scriptText
     .replace(
       /([$a-zA-Z_]+[$a-zA-Z0-9_]*)\s*=\s*function\s*\(/g,
@@ -11,6 +16,6 @@ void (async function Index() {
     .replace(
       /([$a-zA-Z_]+[$a-zA-Z0-9_]*)\s*:\s*function\s*\(/g,
       "$1 : function __dollar_sign__$1(",
-    ).replaceAll('__dollar_sign__','$');
+    ).replaceAll('__dollar_sign__', '$');
   zwriteFileSync("./pretty-pako.js", scriptText);
 })();
